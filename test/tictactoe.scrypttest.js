414c52c7ae904ca20322cd0d81fd0fcd9a4ec893
Xiaohui <whulxh@gmail.com>
2021-04-01 18:25:40 -0700

    refactor test

--- test/tictactoe.scrypttest.js
+++ test/tictactoe.scrypttest.js
@@ -32,15 +32,15 @@ function runCompile(fileName) {
   return result;
 }
 
-const privateKey1 = new bsv.PrivateKey.fromRandom('testnet')
-const publicKey1 = bsv.PublicKey.fromPrivateKey(privateKey1)
+const privateKeyAlice = new bsv.PrivateKey.fromRandom('testnet')
+const publicKeyAlice = bsv.PublicKey.fromPrivateKey(privateKeyAlice)
 
-const privateKey2 = new bsv.PrivateKey.fromRandom('testnet')
-const publicKey2 = bsv.PublicKey.fromPrivateKey(privateKey2)
+const privateKeyBob = new bsv.PrivateKey.fromRandom('testnet')
+const publicKeyBob = bsv.PublicKey.fromPrivateKey(privateKeyBob)
 
 const Tictactoe = buildContractClass(runCompile('tictactoe.scrypt'));
 
-game = new Tictactoe(new PubKey(toHex(publicKey1)), new PubKey(toHex(publicKey2)));
+game = new Tictactoe(new PubKey(toHex(publicKeyAlice)), new PubKey(toHex(publicKeyBob)));
 
 let state = new Bytes('00000000000000000000').toASM();
 game.setDataPart(state)
@@ -48,134 +48,55 @@ game.setDataPart(state)
 describe('Test sCrypt contract Tictactoe In Javascript', () => {
   let result, preimage, sig, prevLockingScript
 
-
-
-  it('n = 0', () => {
-
-    prevLockingScript = game.lockingScript.toASM();
-
-    let newState = new Bytes('01010000000000000000').toASM();
-
-    const tx = newTx();
-    const newLockingScript = [game.codePart.toASM(), newState].join(' ');
-
-
-    tx.addOutput(new bsv.Transaction.Output({
-      script: bsv.Script.fromASM(newLockingScript),
-      satoshis: 10000
-    }))
-
-    preimage = getPreimage(tx, prevLockingScript, inputSatoshis);
-
-    sig = signTx(tx, privateKey1, prevLockingScript, inputSatoshis)
-
-    const context = { tx, inputIndex, inputSatoshis }
-
-    result = game.move(0, new Sig(toHex(sig)), 10000, preimage).verify(context)
-    expect(result.success, result.error).to.be.true;
-
-    game.setDataPart(newState)
-
-  });
-
-
-  it('n = 4', () => {
-    prevLockingScript = game.lockingScript.toASM();
-    let newState = new Bytes('00010000000200000000').toASM();
-    const tx = newTx();
-    const newLockingScript = [game.codePart.toASM(), newState].join(' ');
-
-
-    tx.addOutput(new bsv.Transaction.Output({
-      script: bsv.Script.fromASM(newLockingScript),
-      satoshis: 10000
-    }))
-
-    preimage = getPreimage(tx, prevLockingScript, inputSatoshis);
-
-    sig = signTx(tx, privateKey2, prevLockingScript, inputSatoshis)
-
-    const context = { tx, inputIndex, inputSatoshis }
-
-    result = game.move(4, new Sig(toHex(sig)), 10000, preimage).verify(context)
-    expect(result.success, result.error).to.be.true;
-
-    game.setDataPart(newState)
-
-  });
-
-
-  it('n = 1', () => {
-    prevLockingScript = game.lockingScript.toASM();
-    let newState = new Bytes('01010100000200000000').toASM();
-    const tx = newTx();
-    const newLockingScript = [game.codePart.toASM(), newState].join(' ');
-
-
-    tx.addOutput(new bsv.Transaction.Output({
-      script: bsv.Script.fromASM(newLockingScript),
-      satoshis: 10000
-    }))
-
-    preimage = getPreimage(tx, prevLockingScript, inputSatoshis);
-
-    sig = signTx(tx, privateKey1, prevLockingScript, inputSatoshis)
-
-    const context = { tx, inputIndex, inputSatoshis }
-
-    result = game.move(1, new Sig(toHex(sig)), 10000, preimage).verify(context)
-    expect(result.success, result.error).to.be.true;
-
-    game.setDataPart(newState)
-
-  });
-
-
-  it('n = 8', () => {
-    prevLockingScript = game.lockingScript.toASM();
-    let newState = new Bytes('00010100000200000002').toASM();
-    const tx = newTx();
-    const newLockingScript = [game.codePart.toASM(), newState].join(' ');
-
-
-    tx.addOutput(new bsv.Transaction.Output({
-      script: bsv.Script.fromASM(newLockingScript),
-      satoshis: 10000
-    }))
-
-    preimage = getPreimage(tx, prevLockingScript, inputSatoshis);
-
-    sig = signTx(tx, privateKey2, prevLockingScript, inputSatoshis)
-
-    const context = { tx, inputIndex, inputSatoshis }
-
-    result = game.move(8, new Sig(toHex(sig)), 10000, preimage).verify(context)
-    expect(result.success, result.error).to.be.true;
-
-    game.setDataPart(newState)
-
-  });
-
-
-  it('n = 2', () => {
+  function moveScript(newState) {
+    const state = new Bytes(newState).toASM();
+    const newLockingScript = [game.codePart.toASM(), state].join(' ');
+    return bsv.Script.fromASM(newLockingScript)
+  }
+  
+  function testMove(isAliceTurn, n, outputScript) {
+    const privateKey = isAliceTurn ? privateKeyAlice : privateKeyBob;
     prevLockingScript = game.lockingScript.toASM();
 
     const tx = newTx();
 
     tx.addOutput(new bsv.Transaction.Output({
-      script: bsv.Script.buildPublicKeyHashOut(privateKey1.toAddress()).toHex(),
+      script: outputScript,
       satoshis: 10000
     }))
 
     preimage = getPreimage(tx, prevLockingScript, inputSatoshis);
 
-    sig = signTx(tx, privateKey1, prevLockingScript, inputSatoshis)
+    sig = signTx(tx, privateKey, prevLockingScript, inputSatoshis)
 
     const context = { tx, inputIndex, inputSatoshis }
 
-    result = game.move(2, new Sig(toHex(sig)), 10000, preimage).verify(context)
+    result = game.move(n, new Sig(toHex(sig)), 10000, preimage).verify(context)
     expect(result.success, result.error).to.be.true;
+  }
 
+  it('One full round where Alice wins', () => {
+    // Alice places an X at 0-th cell
+    state = '01010000000000000000';
+    testMove(true, 0, moveScript(state))
+    game.setDataPart(state)
+  
+    // Bob places an O at 4-th cell
+    state = '00010000000200000000';
+    testMove(false, 4, moveScript(state))
+    game.setDataPart(state)
+  
+    // Alice places an X at 1-th cell
+    state = '01010100000200000000';
+    testMove(true, 1, moveScript(state))
+    game.setDataPart(state)
+  
+    // Bob places an O at 8-th cell
+    state = '00010100000200000002';
+    testMove(false, 8, moveScript(state))
+    game.setDataPart(state)
+  
+    // Alice places an X at 2-th cell and wins
+    testMove(true, 2, bsv.Script.buildPublicKeyHashOut(privateKeyAlice.toAddress()));
   });
-
 });
