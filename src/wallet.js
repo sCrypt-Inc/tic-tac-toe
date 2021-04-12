18aac4cbea8358b09149ca7c83f1869273a92dae
hh <zhfnjust@qq.com>
2021-04-12 23:24:57 +0800

    add withdraww button

--- src/wallet.js
+++ src/wallet.js
@@ -2,7 +2,7 @@
 import * as QRCode from 'qrcode.react';
 import { bsv } from 'scryptlib';
 import React, { useState, useEffect } from 'react';
-import { web3, LocalWallet, NetWork } from './web3';
+import { web3, LocalWallet, NetWork, SignType } from './web3';
 import { useInterval } from './hooks';
 import server from './Server';
 
@@ -22,7 +22,7 @@ const Wallet = props => {
                 setBalance(balance)
             })
         } else if (server.getPrivateKey()) {
-            web3.setWallet(new LocalWallet(NetWork.Testnet, server.getPrivateKey()));
+            web3.setWallet(new LocalWallet(NetWork.Mainnet, server.getPrivateKey()));
             web3.wallet.getRawChangeAddress().then(address => {
                 setAddress(address)
             })
@@ -50,10 +50,10 @@ const Wallet = props => {
     const onClick = (e) => {
 
         try {
-            const privateKey = new bsv.PrivateKey.fromRandom('testnet')
+            const privateKey = new bsv.PrivateKey.fromRandom('mainnet')
 
             setAddress(privateKey.toAddress() + '')
-            web3.setWallet(new LocalWallet(NetWork.Testnet, privateKey.toWIF()));
+            web3.setWallet(new LocalWallet(NetWork.Mainnet, privateKey.toWIF()));
 
             server.savePrivateKey(privateKey.toWIF());
         } catch (e) {
@@ -62,6 +62,84 @@ const Wallet = props => {
     };
 
 
+    const onWithdraw = async (e) => {
+
+        try {
+
+            const tx = {
+                inputs: [],
+                outputs: []
+            };
+
+            let withdrawAddress = document.getElementById('withdrawAddress').value;
+
+            let address = new bsv.Address.fromString(withdrawAddress);
+
+            if(address && address.type === 'pubkeyhash' && address.network.name === 'livenet') {
+                console.log('adddress', address )
+            } else {
+                throw 'invalid bitcoin address';
+            }
+            
+            let changeAddress = address + ''
+
+            web3.wallet.listUnspent(0).then(utxos => {
+
+                let total = 0 ;
+                utxos.forEach(utxo => {
+                    total += utxo.satoshis;
+                    tx.inputs.push(
+                        {
+                            utxo: utxo,
+                            script: '',
+                            sequence: 0
+                        }
+                    );
+                })
+
+                const FEE = 500;
+                const changeAmount = total - FEE;
+
+                if (changeAmount <= 0) {
+                    throw new Error('fund is not enough');
+                }
+
+                //add change output
+
+                tx.outputs.push(
+                    {
+                        script: bsv.Script.buildPublicKeyHashOut(changeAddress).toHex(),
+                        satoshis: changeAmount
+                    }
+                );
+
+                return tx;
+            })
+            .then(tx => {
+                //alice sign
+
+                tx.inputs.forEach(async (input, index) => {
+                    let unlockscript = await web3.wallet.signRawTransaction(tx, index, SignType.ALL);
+
+                    tx.inputs[index].script = unlockscript;
+
+                })
+  
+                return tx;
+            }).then(tx => {
+                console.log('send tx', tx)
+                return web3.sendTx(tx);
+            }).then(txid => {
+                console.log('console txid', txid)
+            }).catch(e => {
+                alert('withdraw error ' + e)
+            })
+        
+    } catch(e) {
+        alert('withdraw error ' + e)
+    }
+
+}
 
 
 
@@ -78,10 +156,16 @@ const Wallet = props => {
                 </div>
 
                 <div className="fundtips">
-                    <label >You can fund the address with <a href="https://faucet.bitcoincloud.net" target="_blank"><span> faucets</span></a></label>
+                    <label >You can fund the address with your wallet</label>
                     <br></br>
-                    <label className="warnning">Warnning: Testnet only, don't funds it with your real coin.</label>
+                    <label className="warnning">Warnning:  don't funds it with too much coin.</label>
+                    
                 </div>
+                <div className="withdraw" >
+                    <input id="withdrawAddress" placeholder="Bitcoin Address" type='text' ></input>
+                    <button  onClick={onWithdraw}>Withdraw Fund</button>
+                </div>
+               
             </div>
             <div className="walletqrcode">
                 <QRCode value={address} ></QRCode>
