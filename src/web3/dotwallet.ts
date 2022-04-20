3f7440af17fca8320656debaeee0fc616148c6bc
huliang <hl@boquaninc.com>
2022-04-20 15:08:12 +0800

    update dotwallet

--- src/web3/dotwallet.ts
+++ src/web3/dotwallet.ts
@@ -1,33 +1,42 @@
-import { NetWork, UTXO, wallet, Tx, SignType } from './wallet';
-import axios from 'axios';
-import { DAPP_API_PATHS, getPlayer, getPlayerByState, LocalStorageKey } from '../utils';
-import Request from '../Request';
-
+import { NetWork, UTXO, wallet, Tx, SignType } from "./wallet";
+import { DAPP_API_PATHS, getPlayerByState, LocalStorageKey } from "../utils";
+import Request from "../Request";
 
 export class DotWallet extends wallet {
   API_PREFIX: string;
   API_DOTWALLET: string;
-  CLIENT_ID = 'b02425bfc60c86e8dc1fc0243a005882';
+  CLIENT_ID = "b02425bfc60c86e8dc1fc0243a005882";
   loginUrl: string;
   sender: any;
 
   constructor(network: NetWork = NetWork.Mainnet) {
     super(network);
-    console.log('new DotWallet')
-    this.API_PREFIX = `https://api.whatsonchain.com/v1/bsv/${network === NetWork.Regtest ? 'test' : 'main'}`;
+    console.log("new DotWallet");
+    this.API_PREFIX = `https://api.whatsonchain.com/v1/bsv/${
+      network === NetWork.Regtest ? "test" : "main"
+    }`;
     // this.API_DOTWALLET = network == NetWork.Regtest ?  `http://192.168.1.13:6001` : `https://api.ddpurse.com`;
-    this.API_DOTWALLET = network === NetWork.Regtest ? `http://192.168.1.13:6001` : `https://api.ddpurse.com`;
-    console.log('redirect_uri', `${window.location.origin}/tic-tac-toe`)
-    const loginUrl = `${this.API_DOTWALLET}/authorize?client_id=${this.CLIENT_ID}&redirect_uri=${encodeURIComponent(`${window.location.origin}/tic-tac-toe`)}&response_type=code&scope=${encodeURIComponent("user.info")}`;
+    this.API_DOTWALLET =
+      network === NetWork.Regtest
+        ? `http://192.168.1.13:6001`
+        : `https://api.ddpurse.com`;
+    console.log("redirect_uri", `${window.location.origin}/tic-tac-toe`);
+    const loginUrl = `${this.API_DOTWALLET}/authorize?client_id=${
+      this.CLIENT_ID
+    }&redirect_uri=${encodeURIComponent(
+      `${window.location.origin}/tic-tac-toe`
+    )}&response_type=token&scope=${encodeURIComponent("user.info")}`;
     this.loginUrl = loginUrl;
-    this.sender = network === NetWork.Regtest ? {
-      "appid": "test_bsv_coin_regular",
-      "user_index": 0
-    } : {
-      "appid": "bsv_coin_regular",
-      "user_index": 0
-    }
-
+    this.sender =
+      network === NetWork.Regtest
+        ? {
+            appid: "test_bsv_coin_regular",
+            user_index: 0,
+          }
+        : {
+            appid: "bsv_coin_regular",
+            user_index: 0,
+          };
   }
 
   requestAccount(name: string, permissions: string[]): Promise<any> {
@@ -35,138 +44,173 @@ export class DotWallet extends wallet {
     return Promise.resolve(true);
   }
 
-
-  code2token = async (code: string) => {
-    if (!code) return;
-    try {
-      const { data } = await axios.post(`https://common.mempool.com/api/dotwallet/get_access_token`, {
-        code,
-        redirect_uri: `${window.location.origin}/tic-tac-toe`
-      });
-
-
-      const { access_token } = data.data;
-      if (access_token) {
-        localStorage[LocalStorageKey.accountToken] = access_token;
-        const query = getPlayerByState() == 'alice' ? "?player=alice" : "?player=bob";
-        window.location.href = `${window.location.origin}/tic-tac-toe${query}`
-      }
-    } catch (error) {
-      console.error('code2token error', error)
+  setToken = (token: string) => {
+    if (token) {
+      localStorage[LocalStorageKey.accountToken] = token;
+      const query =
+        getPlayerByState() == "alice" ? "?player=alice" : "?player=bob";
+      window.location.href = `${window.location.origin}/tic-tac-toe${query}`;
     }
   };
 
+  // code2token = async (code: string) => {
+  //   if (!code) return;
+  //   try {
+  //     const { data } = await axios.post(
+  //       `https://common.mempool.com/api/dotwallet/get_access_token`,
+  //       {
+  //         code,
+  //         redirect_uri: `${window.location.origin}/tic-tac-toe`,
+  //       }
+  //     );
+
+  //     const { access_token } = data.data;
+  //     if (access_token) {
+  //       localStorage[LocalStorageKey.accountToken] = access_token;
+  //       const query =
+  //         getPlayerByState() == "alice" ? "?player=alice" : "?player=bob";
+  //       window.location.href = `${window.location.origin}/tic-tac-toe${query}`;
+  //     }
+  //   } catch (error) {
+  //     console.error("code2token error", error);
+  //   }
+  // };
+
   async getbalance(): Promise<number> {
     try {
-      const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_balance}`, {
-        "sender": this.sender,
-      });
+      const { data } = await Request.post(
+        `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_balance}`,
+        {
+          sender: this.sender,
+        }
+      );
       return data.data.confirm + data.data.unconfirm;
     } catch (error) {
       return 0;
     }
   }
 
-  async signRawTransaction(rawtx: string,
+  async signRawTransaction(
+    rawtx: string,
     inputIndex: number,
     sigHashType: SignType,
     addr: string
   ): Promise<string> {
-
-    const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_sign_raw_transaction}`, {
-      "sender": this.sender,
-      "input_index": inputIndex,
-      "sig_type": sigHashType,
-      rawtx,
-      addr,
-    }, {
-      headers: {
-       
+    const { data } = await Request.post(
+      `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_sign_raw_transaction}`,
+      {
+        sender: this.sender,
+        input_index: inputIndex,
+        sig_type: sigHashType,
+        rawtx,
+        addr,
+      },
+      {
+        headers: {},
       }
-    }
     );
-    return data.data.signed_rawtx
+    return data.data.signed_rawtx;
   }
 
-
-
-  async getSignature(rawtx: string,
+  async getSignature(
+    rawtx: string,
     inputIndex: number,
     inputAmount: number,
-    inputScript: string, 
+    inputScript: string,
     sigHashType: SignType,
     addr: string
   ): Promise<string> {
-    const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_signature}`, {
-      "sender": this.sender,
-      "input_index": inputIndex,
-      "sig_type": sigHashType,
-      rawtx,
-      addr,
-    }, {
-      headers: {
-        "player": 'alice'
+    const { data } = await Request.post(
+      `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_signature}`,
+      {
+        sender: this.sender,
+        input_index: inputIndex,
+        sig_type: sigHashType,
+        rawtx,
+        addr,
+      },
+      {
+        headers: {
+          player: "alice",
+        },
       }
-    });
+    );
 
-    return data.data.hex_signature
+    return data.data.hex_signature;
   }
 
   async sendRawTransaction(rawTx: string): Promise<string> {
-
-    const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_send_raw_transaction}`, {
-      "sender": this.sender,
-      rawTx,
-    });
+    const { data } = await Request.post(
+      `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_send_raw_transaction}`,
+      {
+        sender: this.sender,
+        rawTx,
+      }
+    );
 
     return data.data.txid;
   }
 
-  async listUnspent(minAmount: number, options?: { purpose?: string; }): Promise<UTXO[]> {
-    const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_list_unspent}`, {
-      "sender": this.sender,
-      "min_amount": minAmount
-    }, {
-      headers: {
-        "player": options?.purpose
+  async listUnspent(
+    minAmount: number,
+    options?: { purpose?: string }
+  ): Promise<UTXO[]> {
+    const { data } = await Request.post(
+      `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_list_unspent}`,
+      {
+        sender: this.sender,
+        min_amount: minAmount,
+      },
+      {
+        headers: {
+          player: options?.purpose,
+        },
       }
-    });
+    );
 
-    return data.data.utxos.filter((utxo: any) => utxo.satoshis >= minAmount).map((utxo: any) => {
-      const _utxo = {
-        txHash: utxo.tx_hash,
-        outputIndex: utxo.output_index,
-        satoshis: utxo.satoshis,
-        script: utxo.script,
-        addr: utxo.addr,
-        pubkey: utxo.pubkey,
-      } as UTXO;
-      return _utxo;
-    });
+    return data.data.utxos
+      .filter((utxo: any) => utxo.satoshis >= minAmount)
+      .map((utxo: any) => {
+        const _utxo = {
+          txHash: utxo.tx_hash,
+          outputIndex: utxo.output_index,
+          satoshis: utxo.satoshis,
+          script: utxo.script,
+          addr: utxo.addr,
+          pubkey: utxo.pubkey,
+        } as UTXO;
+        return _utxo;
+      });
   }
 
-
-  async getRawChangeAddress(options?: { purpose?: string; }): Promise<string> {
-    const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_raw_change_address}`, {
-      "sender": this.sender,
-    }, {
-      headers: {
-        "player": options?.purpose
+  async getRawChangeAddress(options?: { purpose?: string }): Promise<string> {
+    const { data } = await Request.post(
+      `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_raw_change_address}`,
+      {
+        sender: this.sender,
+      },
+      {
+        headers: {
+          player: options?.purpose,
+        },
       }
-    });
+    );
 
     return data.data.address;
   }
 
-
-  async getPublicKey(options?: { purpose?: string; }): Promise<string> {
-    const { data } = await Request.post(`${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_public_key}`, {
-      "sender": this.sender,
-    }, {
-      headers: {
-        "player": options?.purpose
+  async getPublicKey(options?: { purpose?: string }): Promise<string> {
+    const { data } = await Request.post(
+      `${this.API_DOTWALLET}${DAPP_API_PATHS.dapp_get_public_key}`,
+      {
+        sender: this.sender,
+      },
+      {
+        headers: {
+          player: options?.purpose,
+        },
       }
-    });
+    );
 
     return data.data.public_key;
   }
